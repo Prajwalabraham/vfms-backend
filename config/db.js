109abf4e993586627df8c7ba4d62f1e9ac210744
Prajwalabraham <prajwalabraham.21@gmail.com>
2022-12-27 21:37:28 +0530

    Port Update DB

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
+  port: process.env.PGPORT || 5432,
   password: process.env.PGPASSWORD
   });
   
