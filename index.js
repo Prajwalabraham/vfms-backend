f1540a03a2ddc63f1fa57acbdd452ffdd867c0a8
Prajwalabraham <prajwalabraham.21@gmail.com>
2022-12-27 22:03:47 +0530

    Update Uncaughtexception

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const path = require('path');
 
 dotenv.config()
 
+process.on('uncaughtException', function (err) {
+  console.log(err);
+});
+
 const pool = new Pool({
    user: process.env.PGUSER,
    password: process.env.PGPASSWORD,
